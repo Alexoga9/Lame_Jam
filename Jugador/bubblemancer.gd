e78c825d5e359f84dd2e86f35fdccdeaa7c7c867
Alexoga9 <alexalga32@gmail.com>
2025-01-21 11:41:48 -0400

    Jugador
    Jugador añadido exitosamente con movimiento y animaciones sencillas.

--- Jugador/bubblemancer.gd
+++ Jugador/bubblemancer.gd
@@ -1,1 +1,50 @@
 extends CharacterBody2D
+
+@onready var animacion = %Animacion
+
+@export var velocidad: float
+@export var salto: float
+
+var mirando_derecha = true
+var gravedad = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	mover_x()
+	girar_personaje_al_moverse()
+	saltar(delta)
+	actualizacion_de_animaciones()
+	
+	move_and_slide()
+
+
+func mover_x():
+	var input_axis = Input.get_axis("mover_izquierda", "mover_derecha")
+	velocity.x = input_axis * velocidad
+
+
+func girar_personaje_al_moverse():
+	if (mirando_derecha and velocity.x < 0) or (not mirando_derecha and velocity.x > 0):
+		scale.x *= -1
+		mirando_derecha = not mirando_derecha
+
+
+func saltar(delta):
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = salto
+	
+	if not is_on_floor():
+		velocity.y += gravedad * delta
+
+
+func actualizacion_de_animaciones():
+	if not is_on_floor():
+		if velocity.y <0:
+			animacion.play("Saltar")
+		else:
+			animacion.play("Caida")
+		return
+	
+	if velocity.x:
+		animacion.play("Caminar")
+	else:
+		animacion.play("Idle")
